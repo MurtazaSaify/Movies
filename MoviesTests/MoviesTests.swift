b1bcddf2f1007e35e612bbeff05e99c310ebd93e
Murtuza Saify <murtuza.saify@aptushealth.com>
2020-02-11 09:19:16 +0530

    Added unit tests for Search movies use case.

--- MoviesTests/MoviesTests.swift
+++ MoviesTests/MoviesTests.swift
@@ -9,19 +9,40 @@
 import XCTest
 @testable import Movies
 
-class MoviesTests: XCTestCase {
+class SearchMoviesTests: XCTestCase {
+
+    var searchMovieListWorker: SearchMovieListUseCase?
+    var movies: [Movie] = []
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        searchMovieListWorker = SearchMovieListWorker()
+
+        var m1 = Movie()
+        m1.id = 1
+        m1.title = "Sample Movie 1"
+
+        var m2 = Movie()
+        m1.id = 2
+        m2.title = "Sample Movie 2"
+
+        var m3 = Movie()
+        m3.id = 1
+        m3.title = "Test Movie 3"
+        movies = [m1, m2, m3]
     }
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        searchMovieListWorker = nil
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testSearchMoviesIsFunctioningAsExpected() {
+        guard let searchMovieListWorker = searchMovieListWorker else {
+            fatalError("searchMovieListWorker should be initialized at this time.")
+        }
+        let searchResult = searchMovieListWorker.searchMovieFor(searchTerm: "Sample", from: movies)
+        XCTAssert(searchResult.count == 2)
     }
 
     func testPerformanceExample() {
